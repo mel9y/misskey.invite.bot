4fcb7f142db2ff9eb578177ed5fa24868c80ead6
m2en <me@m2en.dev>
2023-02-04 12:48:36 +0900

    feat: Discord API login logic

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,15 @@
-const node = 'node'.split('').sort().join('');
-console.log(`Hello ${node}!`); // Hello deno!
+import { createBot, GatewayIntents, startBot } from '../deps.ts';
+import { getEnv } from './utils.ts';
+
+const client = createBot({
+  token: getEnv('DISCORD_BOT_TOKEN'),
+  intents: GatewayIntents.Guilds | GatewayIntents.GuildMessages |
+    GatewayIntents.MessageContent,
+  events: {
+    ready(_self, payload) {
+      console.log(`${payload.user.username} is ready!`);
+    },
+  },
+});
+
+await startBot(client);
